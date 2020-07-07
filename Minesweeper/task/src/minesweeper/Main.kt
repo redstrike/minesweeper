e0cdae488449fc0c6449b6cc2bc1e604b9d15a49
Tung Nguyen <tung@vn2rap.com>
2020-07-08 04:58:49 +0700

    feat: stage #2 - flexible mines, allow the player to config the number of mines on the field

--- Minesweeper/task/src/minesweeper/Main.kt
+++ Minesweeper/task/src/minesweeper/Main.kt
@@ -1,12 +1,37 @@
 package minesweeper
 
 import kotlin.random.Random
+import java.util.Scanner
+import kotlin.math.round
 
 fun main() {
-    val field = "........."
-    for (i in 1..9) {
-        val chars = field.toCharArray()
-        chars[Random.nextInt(0, 8)] = 'X'
-        println(chars)
+    val scanner = Scanner(System.`in`)
+    print("How many mines do you want on the field? ")
+    val totalMines = scanner.nextInt()
+    val fieldLine = "........." // 9 safe cells
+    var usedMines = 0
+    var minMines = 0
+    for (line in 1..9) {
+        val lineChars = fieldLine.toCharArray()
+        // Increase min mines per line to ensure total of mines in the worst case (line 1 has no mine)
+        if (line == 2) {
+            val remainingMines = totalMines - usedMines
+            minMines = round(remainingMines.toFloat() / 8).toInt().coerceAtMost(6)
+        }
+        // Placing mines on field line
+        if (usedMines < totalMines) {
+            var mines = Random.nextInt(minMines, 8) // until >= 8
+            while (mines > 0) {
+                if (usedMines == totalMines) break
+                var pos = Random.nextInt(0, 8)
+                while (lineChars[pos] == 'X') {
+                    pos = Random.nextInt(0, 8)
+                }
+                lineChars[pos] = 'X'
+                usedMines++
+                mines--
+            }
+        }
+        println(lineChars)
     }
 }
