c8ba33b2faf92bf264b5fe87e16e6b441dff33a6
Tung Nguyen <tung@vn2rap.com>
2020-07-19 04:12:12 +0700

    feat: stage #5 - battle! add hide mine hints + mark/unmark mine on a masked minefield + free cell command, explore cells and its empty around cells + code refactoring

--- Minesweeper/task/src/minesweeper/Main.kt
+++ Minesweeper/task/src/minesweeper/Main.kt
@@ -1,73 +1,172 @@
 package minesweeper
 
-import kotlin.random.Random
 import java.util.Scanner
 import kotlin.math.round
+import kotlin.random.Random
+
+typealias Field = Array<CharArray>
+
+private const val TOTAL_MINEFIELD_CELLS = 81
+private const val CONGRATS_MESSAGE = "Congratulations! You found all the mines!"
+
+private var totalMines = 0
+private var inGame = true
+private var marksCount = 0
+private var mineMarksCount = 0
+private var exploredCellsCount = 0
 
 fun main() {
-    val scanner = Scanner(System.`in`)
+    // Create minefield
     print("How many mines do you want on the field? ")
-    // Create and print minefield
-    val totalMines = scanner.nextInt()
+    val scanner = Scanner(System.`in`)
+    totalMines = scanner.nextInt()
     val minefield = addMineHints(createMinefield(totalMines))
-    printMinefield(minefield)
+    // Print masked minefield
+    val maskedField = Array(9) { CharArray(9) { '.' } } // 9 safe cells per field line
+    printMinefield(maskedField)
     // Start game
-    startMineSweeper(scanner, minefield, totalMines)
+    startMinesweeper(scanner, minefield, maskedField)
 }
 
-fun startMineSweeper(scanner: Scanner, minefield: Array<CharArray>, totalMines: Int) {
-    var marksCount = 0
-    var mineMarksCount = 0
-    loop@ while (true) {
-        // Get mark coordinates
-        print("Set/delete mines marks (x and y coordinates): ")
+fun startMinesweeper(scanner: Scanner, minefield: Field, maskedField: Field) {
+    while (inGame) {
+        // Get x and y coordinates with a following command (mine/free)
+        print("Set/unset mines marks or claim a cell as free: ")
         val x = scanner.nextInt() - 1
         val y = scanner.nextInt() - 1
-        // Mark or un-mark field cell
-        when (minefield[y][x]) {
-            in '1'..'8' -> {
-                println("There is a number here!")
-                continue@loop
-            }
-            'X' -> {
-                minefield[y][x] = '*'
+        when (scanner.next().toLowerCase()) {
+            "mine" -> markCell(x, y, minefield, maskedField)
+            "free" -> freeCell(x, y, minefield, maskedField)
+        }
+    }
+}
+
+fun markCell(x: Int, y: Int, minefield: Field, maskedField: Field) {
+    val isUnmarkedCell = maskedField[y][x] == '.'
+    val isMarkedCell = maskedField[y][x] == '*' // explored masked cells can be numbers
+    when (minefield[y][x]) {
+        'X' -> {
+            if (isUnmarkedCell) {
+                maskedField[y][x] = '*'
                 marksCount++
                 mineMarksCount++
+            } else if (isMarkedCell) {
+                maskedField[y][x] = '.'
+                marksCount--
+                mineMarksCount--
             }
-            '.' -> {
-                minefield[y][x] = '*'
+        }
+        '.' -> {
+            if (isUnmarkedCell) {
+                maskedField[y][x] = '*'
                 marksCount++
-            }
-            '*' -> {
-                minefield[y][x] = '.'
+            } else if (isMarkedCell) {
+                maskedField[y][x] = '.'
                 marksCount--
             }
         }
-        // Check end game
-        if (mineMarksCount == totalMines && mineMarksCount == marksCount) {
-            return println("Congratulations! You found all the mines!")
+        in '1'..'8' -> {
+            if (isUnmarkedCell) {
+                maskedField[y][x] = '*'
+                marksCount++
+            } else if (isMarkedCell) {
+                maskedField[y][x] = '.'
+                marksCount--
+            }
         }
-        // Print minefield with new state
-        printMinefield(minefield)
+    }
+    // Print masked field with new state
+    printMinefield(maskedField)
+
+    // Check win if the player has marked all mines correctly
+    if (mineMarksCount == totalMines && mineMarksCount == marksCount) {
+        return println(CONGRATS_MESSAGE)
     }
 }
 
-fun printMinefield(minefield: Array<CharArray>) {
+fun freeCell(x: Int, y: Int, minefield: Field, maskedField: Field) {
+    // Explore a cell
+    if (minefield[y][x] == 'X') {
+        maskedField[y][x] = 'X'
+        printMinefield(maskedField)
+        inGame = false
+        return println("You stepped on a mine and failed!")
+    }
+    exploreCell(x, y, minefield, maskedField)
+
+    // Print masked field with new state
+    printMinefield(maskedField)
+
+    // Check win if the player has explored all safe cells
+    if (TOTAL_MINEFIELD_CELLS - exploredCellsCount == totalMines) {
+        inGame = false
+        return println(CONGRATS_MESSAGE)
+    }
+}
+
+fun exploreCell(x: Int, y: Int, minefield: Field, maskedField: Field) {
+    val isUnexploredCell = maskedField[y][x] == '.' || maskedField[y][x] == '*'
+    when (minefield[y][x]) {
+        '.' -> {
+            // This cell is empty and has no mines around it => automatically explore all cells around it,
+            // including the marked ones. Also, explore its next to empty cells with no mines around, recursively.
+            if (isUnexploredCell) {
+                maskedField[y][x] = '/'
+                minefield[y][x] = '/'
+                exploredCellsCount++
+                exploreAroundCells(x, y, minefield, maskedField)
+            }
+        }
+        in '1'..'8' -> {
+            // This cell is empty and has mines around it => only it is explored, revealing a number of mines around it
+            if (isUnexploredCell) {
+                maskedField[y][x] = minefield[y][x]
+                exploredCellsCount++
+            }
+        }
+    }
+}
+
+fun exploreAroundCells(x: Int, y: Int, minefield: Field, maskedField: Field) {
+    val ym1 = y - 1
+    val yp1 = y + 1
+    val xm1 = x - 1
+    val xp1 = x + 1
+    val ym1r = ym1 in 0..8
+    val yp1r = yp1 in 0..8
+    val xm1r = xm1 in 0..8
+    val xp1r = xp1 in 0..8
+    // topCell
+    if (ym1r) exploreCell(x, ym1, minefield, maskedField)
+    // bottomCell
+    if (yp1r) exploreCell(x, yp1, minefield, maskedField)
+    // leftCell
+    if (xm1r) exploreCell(xm1, y, minefield, maskedField)
+    // rightCell
+    if (xp1r) exploreCell(xp1, y, minefield, maskedField)
+    // topLeftCell
+    if (ym1r && xm1r) exploreCell(xm1, ym1, minefield, maskedField)
+    // topRightCell
+    if (ym1r && xp1r) exploreCell(xp1, ym1, minefield, maskedField)
+    // bottomLeftCell
+    if (yp1r && xm1r) exploreCell(xm1, yp1, minefield, maskedField)
+    // bottomRightCell
+    if (yp1r && xp1r) exploreCell(xp1, yp1, minefield, maskedField)
+}
+
+fun printMinefield(minefield: Field) {
+    println()
+    println(" │123456789│")
+    println("—│—————————│")
     for (i in 0..8) {
-        if (i == 0) {
-            println(" │123456789│")
-            println("—│—————————│")
-        }
         print("${i + 1}│")
-        print(minefield[i].joinToString("").replace('X', '.'))
+        print(minefield[i])
         println("│")
-        if (i == 8) {
-            println("—│—————————│")
-        }
     }
+    println("—│—————————│")
 }
 
-fun addMineHints(field: Array<CharArray>): Array<CharArray> {
+fun addMineHints(field: Field): Field {
     val range = 0..8
     for (y in range) {
         for (x in range) {
@@ -105,7 +204,7 @@ fun addMineHints(field: Array<CharArray>): Array<CharArray> {
     return field
 }
 
-fun createMinefield(totalMines: Int): Array<CharArray> {
+fun createMinefield(totalMines: Int): Field {
     val field = Array(9) { CharArray(9) { '.' } } // 9 safe cells per field line
     var minMines = 0
     var usedMines = 0
